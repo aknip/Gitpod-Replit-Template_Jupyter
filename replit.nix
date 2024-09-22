d844c3f8f69e0049dc870673665d00c0cb1536e0
aknip <aknip@web.de>
2024-09-22 18:11:33 +0000

    Replit config

---
+++ replit.nix
@@ -0,0 +1,19 @@
+{pkgs}: {
+  deps = [
+    pkgs.libyaml
+    pkgs.bash
+    pkgs.cacert
+    pkgs.zeromq
+    pkgs.gitFull
+    pkgs.zlib
+    pkgs.xcodebuild
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.libxcrypt
+    pkgs.glibcLocales
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.gdb
+  ];
+}
